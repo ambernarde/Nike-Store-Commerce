59b9162f102d5f1ab15b3ef0064fef1e32e8fa34
Andre <ambernarde@gmail.com>
2023-10-19 23:23:33 -0300

    Adding Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 /*rafec*/
 import React from 'react';
-import { FlexContent, Hero ,Sales, Stories} from './components';
-import { heroapi, popularsales,toprateslaes,highlight,sneaker,story } from './data/data.js';
+import { FlexContent, Hero ,Sales, Stories,Footer, Navbar} from './components';
+import { heroapi, popularsales,toprateslaes,highlight,sneaker,story,footerAPI } from './data/data.js';
+
 
 const App = () => {
   return (
     <>
+    <Navbar/>
       <main className='flex flex-col gap-16 relative'>
          <Hero heroapi={heroapi} />
          <Sales endpoint={popularsales} ifExists />
@@ -14,6 +16,7 @@ const App = () => {
          <FlexContent endpoint={sneaker} />
          <Stories story={story} />
       </main>
+      <Footer footerApi={footerAPI}/>
     </>
   )
 }
