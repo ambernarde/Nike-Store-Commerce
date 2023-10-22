1637f9535fd9f949c20d8546bb41371c8330a015
Andre <ambernarde@gmail.com>
2023-10-22 16:51:50 -0300

    End Section Cart

---
+++ src/app/Store.js
@@ -0,0 +1,13 @@
+import {configureStore, createSlice} from "@reduxjs/toolkit";
+import CartSlice from "./CartSlice";
+
+
+const Store = configureStore({
+    reducer: {
+        cart: CartSlice,
+
+    }
+
+});
+
+export default Store;
